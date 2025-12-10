d9862c3b2ff8936626c20d06e17d83932a312dce
demy de leon <deleondemy99@gmail.com>
2025-12-09 19:49:00 -0600

    Fix DB config for Railway

--- cliente-servidor-base-main/db/db.js
+++ cliente-servidor-base-main/db/db.js
@@ -2,14 +2,14 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE || process.env.DB_NAME,
+  process.env.MYSQLUSER || process.env.DB_USER,
+  process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.MYSQLHOST || process.env.DB_HOST,
     dialect: 'mysql',
-    port: process.env.DB_PORT,
-    logging: false
+    port: process.env.MYSQLPORT || process.env.DB_PORT,
+    logging: false,
   }
 );
 
